1119dc3fb197adb993f098425bfb33af80ef45c4
Sushil Phuyal <63364111+sushilphuyal@users.noreply.github.com>
2024-02-15 15:55:28 +0000

    final edit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sushil Phuyal",
+  title: "Security Researcher",
+  email: "Sushil.Phuyal@studentambassadors.com",
+  gitHub: "sushilphuyal",
+  instagram: "",
+  linkedIn: "1337mickey",
+  medium: "1337mickey",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
